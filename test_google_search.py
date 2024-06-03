7a75dc4dddeb0c444eff104c06ec3494097b2ac8
Ayed-Oukhay <oukhayayed@gmail.com>
2024-06-03 13:08:43 +0200

    adding test script

---
+++ test_google_search.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# Initialize the Chrome driver
+driver = webdriver.Chrome()
+
+# Open Google
+driver.get("http://www.google.com")
+
+# Take a screenshot
+driver.save_screenshot("screenshot1.png")
+
+# Find the search box
+search_box = driver.find_element_by_name("q")
+
+# Type in the search term and submit
+search_box.send_keys("Selenium")
+search_box.send_keys(Keys.RETURN)
+
+# Take a screenshot
+driver.save_screenshot("screenshot2.png")
+
+# Verify the results
+assert "Selenium" in driver.title
+
+# Close the browser
+driver.quit()
